a6c49e2a4f6f32395d63ff85a81f09f8a3bbe6b0
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 06:16:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.innerHTML = 'victory';
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Megan is the champion';
